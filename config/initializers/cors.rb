e588e1ccf651173627056fe636008b41cea636f9
Vladimir <alik.mikhaylov.web@gmail.com>
2022-05-12 21:59:02 +0300

    add site adress on CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://wheel-of-language.vercel.app'
     resource '*',
              headers: :any, methods: [:get, :post, :patch, :delete, :patch, :put, :options],
              expose: ["Authorization"]
