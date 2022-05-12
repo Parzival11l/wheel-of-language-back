e2d6f59332a6387f673743a392ac0f2508ce811c
Vladimir <alik.mikhaylov.web@gmail.com>
2022-05-12 17:25:09 +0300

    CORS settings change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://wheel-of-language-back.vercel.app/'
     resource '*',
              headers: :any, methods: [:get, :post, :patch, :delete, :patch, :put],
              expose: ["Authorization"]
